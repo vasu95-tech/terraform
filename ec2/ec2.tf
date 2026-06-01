5d17339b8317193057f4c1c213a5cacce00f2c50
vasu <vasumattipelli77@gmail.com>
2026-06-01 17:37:22 +0530

    nginx ansible code

---
+++ ec2/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "terraform" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+  
+  tags = {
+    Name = "terraform"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow-all"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+    Name = "allow-all"
+  }
+}
